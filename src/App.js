5182fbabaa4aa80b18dd98d4564583c80e1de3c7
abhisheks <abhisheks@netlink.com>
2020-03-29 21:01:40 +0530

    first changes

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Row, Col } from 'antd';
+import 'antd/dist/antd.css';
+import './App.scss';
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        Hello world
+      </div>
+    );
+  }
+}
+
+export default App;
