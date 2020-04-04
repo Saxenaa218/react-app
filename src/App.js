93862fd1bb1a09d1d04e77f7b0164608b880073c
abhisheks <abhisheks@netlink.com>
2020-04-04 22:20:26 +0530

    chnages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import { Row, Col } from 'antd';
+import { Row, Col, Card } from 'antd';
 import 'antd/dist/antd.css';
 import './App.scss';
 
 class App extends React.Component {
   render () {
     return (
-      <div>
-        Hello world
-      </div>
+      <>
+        <Row>
+          <Col span={8}>
+            <Card style={{ width: 'inherit', height: 'inherit', margin: '20px 10px 20px 20px', borderRadius: 10 }}>
+              <p>Card content</p>
+              <p>Card content</p>
+              <p>Card content</p>
+            </Card> 
+          </Col>
+          <Col span={16}>
+            <Card style={{ width: 'inherit', height: 'inherit', margin: '20px 20px 20px 10px', borderRadius: 10 }}>
+              <p>Card content</p>
+              <p>Card content</p>
+              <p>Card content</p>
+            </Card>
+          </Col>
+        </Row>
+      </>
     );
   }
 }
